d0730c42f428d8959643164f184b2dd4f1134fb4
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 15:52:10 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,83 +1,35 @@
 import streamlit as st
-import time
 
-# 设置页面标题
-st.set_page_config(page_title='时长增加和扣减应用')
-
-# 初始化累计时长
-total_time = 0
-
-# 开始计时
-def start_timing():
-    start_time = time.time()
-    return start_time
-
-# 结束计时
-def end_timing(start_time):
-    end_time = time.time()
-    time_elapsed = end_time - start_time
-    return time_elapsed
-
-# 显示累计时长
-def display_total_time():
-    st.write('累计时长为：', total_time, '秒')
-
-# 主函数
 def main():
-    global total_time
-    st.title('时长增加和扣减应用')
-    add_time_started = False
-    sub_time_started = False
-    add_time_elapsed = 0
-    sub_time_elapsed = 0
-    while True:
-        # 显示累计时长
-        display_total_time()
-        # 开始增加时长
-        if st.button('开始增加'):
-            if not add_time_started:
-                add_time_started = True
-                st.write('开始增加时长')
-                add_time_start_time = start_timing()
-            else:
-                st.write('计时已经开始')
-        # 结束增加时长
-        if st.button('结束增加'):
-            if add_time_started:
-                add_time_started = False
-                st.write('结束增加时长')
-                add_time_elapsed = end_timing(add_time_start_time)
-                total_time += add_time_elapsed
-            else:
-                st.write('请先开始增加时长')
-        # 开始扣减时长
-        if st.button('开始扣减'):
-            if not sub_time_started:
-                sub_time_started = True
-                st.write('开始扣减时长')
-                sub_time_start_time = start_timing()
-            else:
-                st.write('计时已经开始')
-        # 结束扣减时长
-        if st.button('结束扣减'):
-            if sub_time_started:
-                sub_time_started = False
-                st.write('结束扣减时长')
-                sub_time_elapsed = end_timing(sub_time_start_time)
-                total_time -= sub_time_elapsed
-                if total_time < 0:
-                    total_time = 0
-            else:
-                st.write('请先开始扣减时长')
-        # 增加和扣减时长的计时
-        if add_time_started:
-            add_time_elapsed = end_timing(add_time_start_time)
-            st.write('已经增加时长：', add_time_elapsed, '秒')
-        if sub_time_started:
-            sub_time_elapsed = end_timing(sub_time_start_time)
-            st.write('已经扣减时长：', sub_time_elapsed, '秒')
-        # 设置刷新间隔为0.1秒
-        time.sleep(0.1)
+    st.set_page_config(page_title='时长银行')
+    st.title('时长银行')
+    
+    add_time = 0
+    minus_time = 0
+    total_time = 0
 
+    if st.button('开始增加', key='add_start'):
+        add_time = st.session_state.add_time or 0
+        add_time -= st.session_state.minus_time or 0
+        add_time -= st.session_state.add_end_time or 0
+        st.session_state.add_start_time = st.session_state.time_elapsed or 0
+    if st.button('结束增加', key='add_end'):
+        add_end_time = st.session_state.time_elapsed or 0
+        st.session_state.add_end_time = add_end_time
+        total_time += max(0, add_end_time - st.session_state.add_start_time)
+        st.session_state.add_time = add_time
+    if st.button('开始扣减', key='minus_start'):
+        minus_time = st.session_state.minus_time or 0
+        minus_time -= st.session_state.add_time or 0
+        minus_time -= st.session_state.minus_end_time or 0
+        st.session_state.minus_start_time = st.session_state.time_elapsed or 0
+    if st.button('结束扣减', key='minus_end'):
+        minus_end_time = st.session_state.time_elapsed or 0
+        st.session_state.minus_end_time = minus_end_time
+        total_time -= min(0, minus_end_time - st.session_state.minus_start_time)
+        st.session_state.minus_time = minus_time
+        
+    st.write(f'累计时长: {total_time}秒')
+    
 if __name__ == '__main__':
     main()

71d51bf57ee1e468483003910ae5c66e58d9ce77
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 16:49:46 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,60 +1,66 @@
 import streamlit as st
-import datetime
+import time
 
-def main():
-    st.set_page_config(page_title="Time Bank", page_icon="⏰")
+# 设置页面标题
+st.set_page_config(page_title="TimeBank App", page_icon=":hourglass_flowing_sand:")
 
-    # Initialize state variables
-    st.session_state.time_bank = datetime.timedelta(seconds=0)
-    st.session_state.is_adding_time = False
-    st.session_state.is_subtracting_time = False
-    st.session_state.time_since_last_update = datetime.datetime.now()
+# 定义全局变量
+total_time = 0
+adding_time = 0
+subtracting_time = 0
 
-    # Set up layout
-    st.title("Time Bank")
-    st.write("Click the buttons below to add or subtract time.")
-    st.write("Time added is displayed in green, time subtracted in red.")
-    st.write("Current Time Bank Balance:")
-    time_bank_display = st.empty()
-    st.write("")
-
-    add_button = st.button("Add Time")
-    subtract_button = st.button("Subtract Time")
-
-    # Update time bank and display
+# 定义计时器函数
+def timer():
+    global adding_time, subtracting_time
     while True:
-        current_time = datetime.datetime.now()
-        time_delta = current_time - st.session_state.time_since_last_update
-        st.session_state.time_since_last_update = current_time
-        if st.session_state.is_adding_time:
-            st.session_state.time_bank += time_delta
-            time_bank_display.write(
-                f"<div style='color:green;font-size:64px;'>{str(st.session_state.time_bank)}</div>",
-                unsafe_allow_html=True,
-            )
-        elif st.session_state.is_subtracting_time:
-            st.session_state.time_bank -= time_delta
-            time_bank_display.write(
-                f"<div style='color:red;font-size:64px;'>{str(st.session_state.time_bank)}</div>",
-                unsafe_allow_html=True,
-            )
-        else:
-            time_bank_display.write(
-                f"<div style='font-size:64px;'>{str(st.session_state.time_bank)}</div>",
-                unsafe_allow_html=True,
-            )
-        if st.session_state.is_adding_time and not add_button:
-            st.session_state.is_adding_time = False
-            add_button = st.button("Add Time")
-        elif not st.session_state.is_adding_time and add_button:
-            st.session_state.is_adding_time = True
-            add_button = st.button("Stop Adding Time")
-        if st.session_state.is_subtracting_time and not subtract_button:
-            st.session_state.is_subtracting_time = False
-            subtract_button = st.button("Subtract Time")
-        elif not st.session_state.is_subtracting_time and subtract_button:
-            st.session_state.is_subtracting_time = True
-            subtract_button = st.button("Stop Subtracting Time")
+        if adding_time > 0:
+            adding_time += 1
+        if subtracting_time > 0:
+            subtracting_time += 1
+        time.sleep(1)
 
+# 开始计时器
+timer_thread = st.experimental_background_thread(timer)
+
+# 定义主函数
+def main():
+    global total_time, adding_time, subtracting_time
+
+    # 显示标题和页面描述
+    st.title("TimeBank App")
+    st.write("Add or subtract time with the click of a button!")
+
+    # 显示累计时长柱状图和时长数值
+    st.write("")
+    st.write(f"**Total time:** {total_time} seconds")
+    st.progress(total_time/100)
+
+    # 显示添加时长按钮
+    st.write("")
+    if adding_time == 0:
+        add_button = st.button("Start Adding Time")
+    else:
+        add_button = st.button("Stop Adding Time")
+    if add_button:
+        if adding_time == 0:
+            adding_time = 1
+        else:
+            total_time += adding_time
+            adding_time = 0
+
+    # 显示扣减时长按钮
+    st.write("")
+    if subtracting_time == 0:
+        subtract_button = st.button("Start Subtracting Time")
+    else:
+        subtract_button = st.button("Stop Subtracting Time")
+    if subtract_button:
+        if subtracting_time == 0:
+            subtracting_time = 1
+        else:
+            total_time -= subtracting_time
+            subtracting_time = 0
+
+# 运行应用
 if __name__ == "__main__":
     main()

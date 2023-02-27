37c21051bee3b8832ac9f236a7a6c728471aa247
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 16:01:53 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,27 +1,40 @@
 import streamlit as st
+import time
 
 def main():
-    st.title('时长管理系统')
-
-    add_time = st.session_state.get('add_time', 0)
-    sub_time = st.session_state.get('sub_time', 0)
-
+    st.title("时长管理器")
+    add_time = st.session_state.add_time or 0
+    subtract_time = st.session_state.subtract_time or 0
+    total_time = add_time - subtract_time
+    st.write(f"累计时长为：{total_time} 秒")
+    start_time = None
+    end_time = None
+    
     if st.button('开始增加'):
-        st.session_state.add_time = add_time + st.session_state.time if 'time' in st.session_state else add_time
+        start_time = time.time()
 
     if st.button('结束增加'):
-        st.session_state.add_time = add_time + st.session_state.time - st.session_state.get('start_time', 0) if 'time' in st.session_state and 'start_time' in st.session_state else add_time
+        end_time = time.time()
+
+    if start_time is not None and end_time is not None:
+        duration = int(end_time - start_time)
+        add_time += duration
+        st.session_state.add_time = add_time
+        total_time = add_time - subtract_time
+        st.write(f"累计时长为：{total_time} 秒")
 
     if st.button('开始扣减'):
-        st.session_state.sub_time = sub_time + st.session_state.time if 'time' in st.session_state else sub_time
+        start_time = time.time()
 
     if st.button('结束扣减'):
-        st.session_state.sub_time = sub_time + st.session_state.time - st.session_state.get('start_time', 0) if 'time' in st.session_state and 'start_time' in st.session_state else sub_time
+        end_time = time.time()
 
-    st.write(f'累计时长：{add_time - sub_time} 秒')
-
-    if st.button('重置'):
-        st.session_state.clear()
+    if start_time is not None and end_time is not None:
+        duration = int(end_time - start_time)
+        subtract_time += duration
+        st.session_state.subtract_time = subtract_time
+        total_time = add_time - subtract_time
+        st.write(f"累计时长为：{total_time} 秒")
 
 if __name__ == '__main__':
     main()

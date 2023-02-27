da902997b3359cf90ac241b681c9311bdace705e
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 15:58:36 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,37 +1,27 @@
 import streamlit as st
-st.session_state.my_var = 0
-st.session_state['add_time'] = st.session_state.get('add_time', 0) + duration
 
 def main():
-    st.set_page_config(page_title='时长银行')
-    st.title('时长银行')
-    
-    add_time = 0
-    minus_time = 0
-    total_time = 0
+    st.title('时长管理系统')
 
-    if st.button('开始增加', key='add_start'):
-        add_time = st.session_state.add_time or 0
-        add_time -= st.session_state.minus_time or 0
-        add_time -= st.session_state.add_end_time or 0
-        st.session_state.add_start_time = st.session_state.time_elapsed or 0
-    if st.button('结束增加', key='add_end'):
-        add_end_time = st.session_state.time_elapsed or 0
-        st.session_state.add_end_time = add_end_time
-        total_time += max(0, add_end_time - st.session_state.add_start_time)
-        st.session_state.add_time = add_time
-    if st.button('开始扣减', key='minus_start'):
-        minus_time = st.session_state.minus_time or 0
-        minus_time -= st.session_state.add_time or 0
-        minus_time -= st.session_state.minus_end_time or 0
-        st.session_state.minus_start_time = st.session_state.time_elapsed or 0
-    if st.button('结束扣减', key='minus_end'):
-        minus_end_time = st.session_state.time_elapsed or 0
-        st.session_state.minus_end_time = minus_end_time
-        total_time -= min(0, minus_end_time - st.session_state.minus_start_time)
-        st.session_state.minus_time = minus_time
-        
-    st.write(f'累计时长: {total_time}秒')
-    
+    add_time = st.session_state.get('add_time', 0)
+    sub_time = st.session_state.get('sub_time', 0)
+
+    if st.button('开始增加'):
+        st.session_state.add_time = add_time + st.session_state.time if 'time' in st.session_state else add_time
+
+    if st.button('结束增加'):
+        st.session_state.add_time = add_time + st.session_state.time - st.session_state.get('start_time', 0) if 'time' in st.session_state and 'start_time' in st.session_state else add_time
+
+    if st.button('开始扣减'):
+        st.session_state.sub_time = sub_time + st.session_state.time if 'time' in st.session_state else sub_time
+
+    if st.button('结束扣减'):
+        st.session_state.sub_time = sub_time + st.session_state.time - st.session_state.get('start_time', 0) if 'time' in st.session_state and 'start_time' in st.session_state else sub_time
+
+    st.write(f'累计时长：{add_time - sub_time} 秒')
+
+    if st.button('重置'):
+        st.session_state.clear()
+
 if __name__ == '__main__':
     main()

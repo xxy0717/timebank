35dca19dd8573684729efdf51b1f853d507c0dfb
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 16:08:35 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,22 +1,73 @@
 import streamlit as st
+import time
+
 
 def main():
-    add_time = st.session_state.get('add_time', 0) # 使用get方法获取变量，如果不存在则返回默认值0
-    subtract_time = st.session_state.get('subtract_time', 0)
-    
-    if st.button('开始增加'):
-        st.session_state.add_time = add_time
-        st.session_state.start_time = st.session_state.time if 'time' in st.session_state else 0
-    if st.button('结束增加'):
-        add_time += st.session_state.time - st.session_state.start_time
-        st.session_state.add_time = add_time
-    
-    if st.button('开始扣减'):
-        st.session_state.subtract_time = subtract_time
-        st.session_state.start_time = st.session_state.time if 'time' in st.session_state else 0
-    if st.button('结束扣减'):
-        subtract_time += st.session_state.time - st.session_state.start_time
-        st.session_state.subtract_time = subtract_time
-    
-    total_time = add_time - subtract_time
-    st.write(f'累计时长: {total_time} 秒')
+    st.set_page_config(page_title='Time Bank', page_icon=':money_with_wings:')
+    st.title('Time Bank')
+
+    add_time = 0
+    deduct_time = 0
+    is_adding = False
+    is_deducting = False
+
+    if 'add_time' in st.session_state:
+        add_time = st.session_state.add_time
+
+    if 'deduct_time' in st.session_state:
+        deduct_time = st.session_state.deduct_time
+
+    if 'is_adding' in st.session_state:
+        is_adding = st.session_state.is_adding
+
+    if 'is_deducting' in st.session_state:
+        is_deducting = st.session_state.is_deducting
+
+    add_button = st.button('开始增加')
+    deduct_button = st.button('开始扣减')
+
+    if add_button and not is_adding:
+        is_adding = True
+        st.session_state.is_adding = is_adding
+        with st.empty():
+            t0 = time.time()
+            while is_adding:
+                t1 = time.time()
+                add_time += t1 - t0
+                t0 = t1
+                st.session_state.add_time = add_time
+                st.session_state.is_adding = is_adding
+                st.write(f'累计时长：{add_time:.2f}秒')
+                time.sleep(0.1)
+
+    if deduct_button and not is_deducting:
+        is_deducting = True
+        st.session_state.is_deducting = is_deducting
+        with st.empty():
+            t0 = time.time()
+            while is_deducting:
+                t1 = time.time()
+                deduct_time += t1 - t0
+                t0 = t1
+                st.session_state.deduct_time = deduct_time
+                st.session_state.is_deducting = is_deducting
+                st.write(f'累计时长：{add_time - deduct_time:.2f}秒')
+                time.sleep(0.1)
+
+    if is_adding:
+        end_add_button = st.button('结束增加')
+        if end_add_button:
+            is_adding = False
+            st.session_state.is_adding = is_adding
+
+    if is_deducting:
+        end_deduct_button = st.button('结束扣减')
+        if end_deduct_button:
+            is_deducting = False
+            st.session_state.is_deducting = is_deducting
+
+    st.write(f'累计时长：{add_time - deduct_time:.2f}秒')
+
+
+if __name__ == '__main__':
+    main()

b667be54158ee2a80eed4e606d1af9b0c9084452
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 16:34:57 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,47 +1,49 @@
 import streamlit as st
-import time
+from streamlit import session_state as state
+import datetime
 
 def main():
-    # Set initial time and status
-    current_time = 0
-    is_adding_time = False
-
-    # Set up Streamlit app
-    st.set_page_config(page_title="Time Bank", page_icon=":money_with_wings:")
     st.title("Time Bank")
 
-    # Create bar chart to display time
-    time_chart = st.bar_chart({"Time": current_time})
+    # Initialize session state variables
+    state.start_adding = False
+    state.start_deducting = False
+    state.total_time = datetime.timedelta(seconds=0)
 
-    # Create "Add Time" button
-    add_button = st.button("Start Adding Time")
+    # Create the "Add Time" button
+    add_button_text = "Start Adding Time" if not state.start_adding else "Stop Adding Time"
+    add_button = st.button(add_button_text)
 
-    # Create "Subtract Time" button
-    subtract_button = st.button("Start Subtracting Time")
+    # Create the "Deduct Time" button
+    deduct_button_text = "Start Deducting Time" if not state.start_deducting else "Stop Deducting Time"
+    deduct_button = st.button(deduct_button_text)
 
-    # Update time on each iteration
-    while True:
-        if is_adding_time:
-            current_time += 1
-            time_chart.add_rows({"Time": current_time})
+    # Create the "Reset Time" button
+    reset_button = st.button("Reset Time")
 
-        # Check if "Add Time" button has been clicked
-        if add_button:
-            # Toggle adding time on or off
-            is_adding_time = not is_adding_time
+    # Calculate total time
+    if state.start_adding:
+        state.total_time += datetime.timedelta(seconds=1)
+    if state.start_deducting:
+        state.total_time -= datetime.timedelta(seconds=1)
 
-            if is_adding_time:
-                add_button.button("Stop Adding Time")
-            else:
-                add_button.button("Start Adding Time")
+    # Update the UI
+    st.write(f"Total Time: {state.total_time}")
+    st.bar_chart({"total time": state.total_time.total_seconds()})
 
-        # Check if "Subtract Time" button has been clicked
-        if subtract_button:
-            current_time -= 1
-            time_chart.add_rows({"Time": current_time})
+    # Handle button clicks
+    if add_button_text == "Start Adding Time":
+        state.start_adding = True
+    else:
+        state.start_adding = False
 
-        # Wait 1 second before updating time again
-        time.sleep(1)
+    if deduct_button_text == "Start Deducting Time":
+        state.start_deducting = True
+    else:
+        state.start_deducting = False
+
+    if reset_button:
+        state.total_time = datetime.timedelta(seconds=0)
 
 if __name__ == "__main__":
     main()

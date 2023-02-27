e91f11401ff5b43af7d57f61ebad681f91e60fe9
xxy0717 <125196979+xxy0717@users.noreply.github.com>
2023-02-27 16:53:40 +0800

    Update timebank.py

--- timebank.py
+++ timebank.py
@@ -1,66 +1,90 @@
 import streamlit as st
 import time
+import threading
 
-# 设置页面标题
-st.set_page_config(page_title="TimeBank App", page_icon=":hourglass_flowing_sand:")
+# Set page title
+st.set_page_config(page_title="Time Bank")
 
-# 定义全局变量
+# Define initial values
 total_time = 0
-adding_time = 0
-subtracting_time = 0
+time_unit = "seconds"
+add_button_text = "Start Adding Time"
+subtract_button_text = "Start Subtracting Time"
 
-# 定义计时器函数
+# Define helper function to convert seconds to specified time unit
+def convert_time(seconds, time_unit):
+    if time_unit == "seconds":
+        return seconds
+    elif time_unit == "minutes":
+        return seconds / 60
+    elif time_unit == "hours":
+        return seconds / 3600
+    elif time_unit == "days":
+        return seconds / 86400
+
+# Define function to update total time
+def update_total_time(change):
+    global total_time
+    total_time += change
+
+# Define function to run timer in background
 def timer():
-    global adding_time, subtracting_time
     while True:
-        if adding_time > 0:
-            adding_time += 1
-        if subtracting_time > 0:
-            subtracting_time += 1
         time.sleep(1)
+        update_total_time(1)
 
-# 开始计时器
-timer_thread = st.experimental_background_thread(timer)
+# Define function to start and stop adding time
+def add_time():
+    global add_button_text
+    if add_button_text == "Start Adding Time":
+        add_button_text = "Stop Adding Time"
+        timer_thread = st.experimental_background_thread(timer)
+    else:
+        add_button_text = "Start Adding Time"
 
-# 定义主函数
+# Define function to start and stop subtracting time
+def subtract_time():
+    global subtract_button_text
+    if subtract_button_text == "Start Subtracting Time":
+        subtract_button_text = "Stop Subtracting Time"
+        timer_thread = st.experimental_background_thread(timer)
+    else:
+        subtract_button_text = "Start Subtracting Time"
+
+# Define Streamlit app
 def main():
-    global total_time, adding_time, subtracting_time
+    global total_time, time_unit, add_button_text, subtract_button_text
 
-    # 显示标题和页面描述
-    st.title("TimeBank App")
-    st.write("Add or subtract time with the click of a button!")
+    # Add title and description
+    st.title("Time Bank")
+    st.write("This app allows you to add and subtract time.")
 
-    # 显示累计时长柱状图和时长数值
-    st.write("")
-    st.write(f"**Total time:** {total_time} seconds")
-    st.progress(total_time/100)
+    # Add slider to select time unit
+    time_unit = st.sidebar.selectbox("Select time unit:", ["seconds", "minutes", "hours", "days"])
 
-    # 显示添加时长按钮
-    st.write("")
-    if adding_time == 0:
-        add_button = st.button("Start Adding Time")
-    else:
-        add_button = st.button("Stop Adding Time")
+    # Add total time display
+    st.write(f"Total time: {convert_time(total_time, time_unit):,.2f} {time_unit}")
+
+    # Add adding and subtracting buttons
+    add_button = st.button(add_button_text)
+    subtract_button = st.button(subtract_button_text)
+
+    # Update total time and button text based on adding button click
     if add_button:
-        if adding_time == 0:
-            adding_time = 1
-        else:
-            total_time += adding_time
-            adding_time = 0
+        add_time()
+        if add_button_text == "Start Adding Time":
+            update_total_time(1)
+        st.experimental_rerun()
 
-    # 显示扣减时长按钮
-    st.write("")
-    if subtracting_time == 0:
-        subtract_button = st.button("Start Subtracting Time")
-    else:
-        subtract_button = st.button("Stop Subtracting Time")
+    # Update total time and button text based on subtracting button click
     if subtract_button:
-        if subtracting_time == 0:
-            subtracting_time = 1
-        else:
-            total_time -= subtracting_time
-            subtracting_time = 0
+        subtract_time()
+        if subtract_button_text == "Start Subtracting Time":
+            update_total_time(-1)
+        st.experimental_rerun()
 
-# 运行应用
+    # Add progress bar to represent total time
+    st.progress(convert_time(total_time, time_unit) / 86400)
+
 if __name__ == "__main__":
     main()
